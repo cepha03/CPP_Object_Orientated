6bb3873d40d5bc9198516ee49a2faa32b851e592
Aqil Khan <aqilk1347@gmail.com>
2026-01-13 18:30:19 +0000

    added board function to ensure all components are correctly placed on board.

--- cpp_oo.cpp
+++ cpp_oo.cpp
@@ -18,4 +18,29 @@ private:
     time_t startTime;
     int flips;//tracks how many times we've flipped
     bool gameOver;
+
+    //function for board object. variables to be only used by initBoard
+    void initBoard(){
+
+        //assign board components 
+        board.assign(size, vector<char>(size, '.'));
+        revealed.assign(size, vector<bool>(size, false));
+        bombLoc.clear();
+
+        //bombs are to be randomly scattered and not fall in the same block
+        srand(time(0));//random number generator. ensures bombs are not in the same block every game
+        int bombsPlaced = 0;
+        while (bombsPlaced < numBombs) { //loop to place all 10 bombs
+            int r = rand() % size; //set coordinates for each bomb. rand() generates a large number. 
+            int c = rand() % size; //modulus size of board (remainder)
+            if (bombLoc.find({r, c}) == bombLoc.end()) { //list bombloc 
+                bombLoc.insert({r, c}); //bomb inserted in correct coordinate
+                bombsPlaced++;
+            }
+        }
+        //time set to 0 when game starts. 
+        startTime = time(0);
+        flips = 0; //flips set to 0 (loss in gravity)
+        cout << "Game has started! Bombs hidden: " << numBombs << endl;
+    }
 };
